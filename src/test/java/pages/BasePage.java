cb2990586cbd47f8e45444699376d50277562695
Alejandra Lopez <alexeindev@gmail.com>
2022-10-12 11:06:32 -0500

    Added methods to set elements

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class BasePage {
@@ -37,8 +34,44 @@ public class BasePage {
     public WebElement titleBreadcrumb;
 
     public boolean verifyBreadcrumbTitle(String breadcrumbTitle) {
-       String currentTitle = titleBreadcrumb.getText();
-       return breadcrumbTitle.equals(currentTitle);
+        String currentTitle = titleBreadcrumb.getText();
+        return breadcrumbTitle.equals(currentTitle);
     }
 
+    public WebElement getTbxInputElement(String labelName) {
+        return driver.findElement(By.xpath("//label[normalize-space()='%s']/../following-sibling::div/input".formatted(labelName)));
+    }
+
+    public void sendKeysToTextBox(String labelName, String value) {
+        driver.findElement(By.xpath("//label[normalize-space()='%s']/../following-sibling::div/input".formatted(labelName))).sendKeys(value);
+    }
+
+    public void setDropDownByText(String labelName, String optionToSelect) {
+        WebElement dropdownElement = driver.findElement(By.xpath("//label[normalize-space()='%s']/../following-sibling::div".formatted(labelName)));
+        dropdownElement.click();
+        WebElement option = driver.findElement(By.xpath("//div[@role='option']/span[contains(text(),'%s')]".formatted(optionToSelect)));
+        option.click();
+    }
+
+    public void setAutocompleteByText(String labelName, String optionToSelect) {
+        WebElement autocompleteElement = driver.findElement(By.xpath("//label[normalize-space()='%s']/../following-sibling::div/div/div/input".formatted(labelName)));
+        String[] slicedString = optionToSelect.split(" ");
+        String formattedOption = String.join("  ", slicedString);
+        autocompleteElement.sendKeys(optionToSelect.substring(0,2));
+        WebElement option = driver.findElement(By.xpath("//div[@role='option']/span[contains(text(),'%s')]".formatted(formattedOption)));
+        option.click();
+    }
+
+    public class Field {
+
+        private String fieldName;
+        private String fieldType;
+        private String value;
+
+        public Field(String fieldName, String fieldType, String value) {
+            this.fieldName = fieldName;
+            this.fieldType = fieldType;
+            this.value = value;
+        }
+    }
 }
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -4,26 +4,33 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
+import pages.BasePage;
 import pages.LoginPage;
 
 import static stepdefinitions.Hooks.driver;
 
 
 public class LoginSteps {
-    LoginPage loginPage;
+    BasePage basePage = new BasePage(driver);
+    LoginPage loginPage = new LoginPage(driver);
 
     @Given("the user goes to the login page")
     public void theUserGoesToTheLoginPage() {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
+
     @When("the user sets the correct credentials")
     public void theUserSetsTheCorrectCredentials() {
-        loginPage = new LoginPage(driver);
         loginPage.logUserIn("Admin", "admin123");
     }
-    @Then("the user should be logged in the application")
-    public void theUserShouldBeLoggedInTheApplication() {
-        Assert.assertTrue(loginPage.verifyBreadcrumbTitle("PIM"));
+
+    @When("^the user navigates to the \"(.*)\" page$")
+    public void theUserNavigatesToThePage(String pageName) {
+        basePage.navigateWithSideMenu(pageName);
     }
 
+    @Then("^the \"(.*)\" page should be displayed$")
+    public void thePageShouldBeDisplayed(String pageName) {
+        Assert.assertTrue(loginPage.verifyBreadcrumbTitle(pageName));
+    }
 }
